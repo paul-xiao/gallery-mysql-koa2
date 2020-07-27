1761290b29f5d13f9805ce15ae95e20a45cea588
paul.xiao <375197736@qq.com>
2020-07-27 10:33:44 +0800

    add sequelize-cli

--- app/models/user.js
+++ app/models/user.js
@@ -1,30 +1,26 @@
-const { Sequelize } = require('sequelize')
-const db = require('../config/db')
-const sequelize = new Sequelize(db.database, db.user, db.password, {
-  host: db.host,
-  port: db.port,
-  dialect: 'mysql',
-})
-
-const User = sequelize.define(
-  // 默认表名（一般这里写单数），生成时会自动转换成复数形式
-  // 这个值还会作为访问模型相关的模型时的属性名，所以建议用小写形式
-  'user',
-  // 字段定义（主键、created_at、updated_at默认包含，不用特殊定义）
-  {
-    emp_id: {
-      type: Sequelize.CHAR(10), // 字段类型
-      allowNull: false, // 是否允许为NULL
-      unique: true, // 字段是否UNIQUE
-    },
-    nick: {
-      type: Sequelize.CHAR(10),
-      allowNull: false,
-    },
-    department: {
-      type: Sequelize.STRING(64),
-      allowNull: true,
-    },
-  }
-)
-module.exports = User
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  User.init({
+    username: DataTypes.STRING,
+    avatar: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
---
+++ app/seeders/20200727020035-user.js
@@ -0,0 +1,38 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert(
+      'Users',
+      [
+        {
+          username: 'paul',
+          avatar: '',
+          email: 'paulxiaooo@gmail.com',
+          password: '123456',
+          createdAt: '2020-07-27',
+          updatedAt: '2020-07-27',
+        },
+      ],
+      {}
+    )
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+}
